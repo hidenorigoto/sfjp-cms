3fb33dfd8d1046b3811d9b5dfe5b25d21e653d30
Hidenori Goto <hidenorigoto@gmail.com>
2010-06-24 01:32:54 +0900

    blog一覧処理用のモジュールを追加

---
+++ apps/frontend/modules/blog/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * blog actions.
+ *
+ * @package    sfjp-cms
+ * @subpackage blog
+ * @author     hidenorigoto
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class blogActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
