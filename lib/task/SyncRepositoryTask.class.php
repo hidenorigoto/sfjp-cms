4aec1c5fa4554ab0627a3ad037cba595660e591c
Hidenori Goto <hidenorigoto@gmail.com>
2010-07-05 17:29:21 +0900

    初回コミット日時を設定するように修正

--- lib/task/SyncRepositoryTask.class.php
+++ lib/task/SyncRepositoryTask.class.php
@@ -308,6 +308,11 @@ EOF;
             // ページの最終コミット日時を更新する。
             $commit = CommitTable::getLatestCommit($page->getId());
             $page->setLastUpdated($commit->getCommittedAt());
+
+            // ページの最初のコミット日時を更新する。
+            $commit = CommitTable::getFirstCommit($page->getId());
+            $page->setFirstCommitted($commit->getCommittedAt());
+
             $page->save();
         }
 
