8460ceba10f2d5d708bd4358686192867d3b48c1
Hidenori Goto <hidenorigoto@gmail.com>
2010-07-05 17:26:47 +0900

    pageレコードに初回コミット年月を設定するタスクを追加

---
+++ lib/task/setfirstcommittedymTask.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class setFirstCommittedYmTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'sfjp';
+    $this->name             = 'set-first-committed-ym';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [set-first-committed-ym|INFO] task does things.
+Call it with:
+
+  [php symfony set-first-committed-ym|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+      $pages = PageTable::getInstance()->findAll();
+      foreach ($pages as $page) {
+          $commit = CommitTable::getFirstCommit($page->getId());
+          $page->setFirstCommittedYm((int)$commit->getDateTimeObject('committed_at')->format('Ym'));
+
+          $page->save();
+      }
+  }
+}
